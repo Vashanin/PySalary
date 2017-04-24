5ccf91c8c5be466b6f999f825d97454b496454bf
Vashanin <vashanin7@gmail.com>
2017-04-24 22:57:15 +0300

    Add HTML

--- __init__.py
+++ __init__.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 def homepage():
     return render_template("main.html")
 
+@app.route('/employees/')
+def employees():
+    return render_template("employees.html")
+
+@app.route('/posts/')
+def posts():
+    return render_template("posts.html")
+
+@app.route("/tables/")
+def tables():
+    return render_template("tables.html")
+
+@app.route("/salaries/")
+def salaries():
+    return render_template("salaries.html")
+
+@app.route("/new-table/")
+def new_table():
+    return("Here must be adding form")
 
 if __name__ == "__main__":
     app.run()
